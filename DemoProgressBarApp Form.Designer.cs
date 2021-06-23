991387cea3b4865f9f356b275b771c5a93ec8565
DANIEL AGBAJI <agbajidaniel@hotmail.com>
2021-06-23 18:18:34 -0500

    Add project files.

---
+++ DemoProgressBarApp Form.Designer.cs
@@ -0,0 +1,129 @@
+﻿
+namespace DemoProgressBarApp
+{
+    partial class ProgressBarAppForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(-1, 12);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(1085, 131);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.radioButton1.Location = new System.Drawing.Point(149, 218);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(288, 43);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.Text = "Show, 1,2,3,4,5";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.rdbProgressorArray_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.radioButton2.Location = new System.Drawing.Point(505, 218);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(288, 43);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.Text = "Show, 5,4,3,2,1";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.rdbRegressorArray_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.button1.Location = new System.Drawing.Point(81, 305);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(306, 105);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Fill";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(582, 305);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(306, 105);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(-1, 461);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(1085, 352);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            // 
+            // ProgressBarAppForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1085, 813);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.progressBar1);
+            this.Name = "ProgressBarAppForm";
+            this.Text = "Progress Bar App";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ DemoProgressBarApp Form.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace DemoProgressBarApp
+{
+    public partial class ProgressBarAppForm : Form
+    {
+        public ProgressBarAppForm()
+        {
+            InitializeComponent();
+        }
+
+        private void rdbProgressorArray_CheckedChanged(object sender, EventArgs e)
+        {
+            //<summary>
+            //Create an array of numbers, then iterate through the array to display it's members
+            //</summary>
+            // Loop through the various range value for the progress and display the step
+            progressBar1.Value = 0;
+            for (int count = progressBar1.Minimum; count <= progressBar1.Maximum; count++)
+            {
+                progressBar1.PerformStep();
+            }
+            richTextBox1.Text = "";
+            var counter = 1;
+
+            while (counter<=5)
+            {
+                richTextBox1.Text += $"X={counter}\n";// Each time a new value is shown for the range, create a new line for the next
+                counter++;
+            }
+
+        }
+
+        private void rdbRegressorArray_CheckedChanged(object sender, EventArgs e)
+        {
+            //<Summary>
+            //Do a reverse enginerring of what we already have in the rdbProgressorArray_CheckedChanged method
+            //</Summary>
+            progressBar1.Value = 0;
+            for (int count = progressBar1.Minimum; count <= (progressBar1.Value=80); count++)
+            {
+                progressBar1.PerformStep();
+            }
+            richTextBox1.Text = "";
+            var counter = 3;
+            while (counter >= 1)
+            {
+                richTextBox1.Text += $"X={counter}\n";
+                counter--;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (richTextBox1.Text !=string.Empty || progressBar1.Value==100)
+            {
+                richTextBox1.Clear();
+                progressBar1.Value=0;
+            }
+            else
+            {
+                MessageBox.Show("Sorry, you don't have data in the rich text box area");
+            }
+        }
+    }
+}
